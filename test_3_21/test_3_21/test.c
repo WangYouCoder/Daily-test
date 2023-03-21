f58ea033fb8e60f81b639edfae0b5c595d1dd1c0
WangYouCoder <123453572+WangYouCoder@users.noreply.github.com>
2023-03-21 22:14:53 +0800

    每日一题

---
+++ test_3_21/test_3_21/test.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//struct S1
+//{
+//    char c1;
+//    char c2;
+//    int i;
+//};
+//struct S2
+//{
+//    char c1;
+//    struct S1 s1;
+//    double d;
+//};
+//int main()
+//{
+//    printf("%d\n", sizeof(struct S2));
+//    return 0;
+//}
+//#pragma pack(8)//设置默认对齐数为8
+//struct S1
+//{
+//    char c1;
+//    int i;
+//    char c2;
+//};
+//#pragma pack()//取消设置的默认对齐数，还原为默认
+//#pragma pack(1)//设置默认对齐数为1
+//struct S2
+//{
+//    char c1;
+//    int i;
+//    char c2;
+//};
+//#pragma pack()//取消设置的默认对齐数，还原为默认
+//int main()
+//{
+//    //输出的结果是什么？
+//    printf("%d\n", sizeof(struct S1));
+//    printf("%d\n", sizeof(struct S2));
+//    return 0;
+//}
+
+//struct S
+//{
+//	char a : 3;
+//	char b : 4;
+//	char c : 5;
+//	char d : 4;
+//};
+//int main()
+//{
+//	struct S s = { 0 };
+//	s.a = 10;
+//	s.b = 12;
+//	s.c = 3;
+//	s.d = 4;
+//	return 0;
+//}
+
