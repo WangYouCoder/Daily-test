4797058eaa5f7975d8a10161fa89921ec7a82c7e
WangYouCoder <123453572+WangYouCoder@users.noreply.github.com>
2023-07-03 18:06:18 +0800

    每日一题

---
+++ test_7_3/test_7_3/test.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+//int main()
+//{
+//	int curVal = 0, val = 0;
+//	//cin >> curVal;
+//	if (cin>>curVal)
+//	{
+//		int cnt = 1;
+//		while (cin >> val)
+//		{
+//			if (curVal == val)
+//			{
+//				cnt++;
+//			}
+//			else
+//			{
+//				cout << curVal << " occurs " << cnt << " time " << endl;
+//				curVal = val;
+//				cnt = 1;
+//			}
+//		}
+//		cout << curVal << " occurs " << cnt << " time " << endl;
+//	}
+//	return 0;
+//}
+
+
+///*
+//* 注释对/* */不能嵌套
+//* “不能嵌套”几个字会被当作源码，
+//* 像剩余程序一样处理
+//*/
+
+//int main()
+//{
+//	cout << "/*";
+//	cout << "*/";
+//	cout <</* "*/" */;
+//	cout <</* "*/"/*"/* " */;
+//	return 0;
+//}
+
+int main()
+{
+	unsigned char c = -1;
+	cout << c;
+	return 0;
+}
